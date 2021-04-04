30ebfade4921510b61492aab752a93bafa3c2bdf
NOEL <noel@Mac-mini-de-NOEL.local>
2021-04-04 22:30:31 +0200

    commit_II_04_04_2021

--- queries.sql
+++ queries.sql
@@ -1,34 +1,31 @@
 --1
 SELECT * FROM salesman;
-
 --3
 SELECT 5, 10, 15;
-
 --4 
 SELECT 10 + 15;
-
 --5
 SELECT 10 + 15 - 5 * 2;
-
 --6
 SELECT commission, name FROM salesman;
-
 --7
 SELECT ord_data as orderdate, salesman_id as salesman id, ord_no order as number, purch_amt as purchase amount from orders;
-
 --8
 SELECT ord_date, salesman_id, ord_no, purch_amt
 FROM orders;
-
 --9
 SELECT name, city FROM salesman WHERE city = 'Paris';
-
 --10
 SELECT * FROM customer WHERE grade = 200;
-
 --11
 SELECT ord_no, ord_date, purch_amt
 FROM orders
 WHERE salesman_id=5001;
-
+--12
+SELECT subject, winner FROM nobel_win WHERE year = 1970;
+--13
+SELECT * FROM nobel_win WHERE year = 1970 AND category = 'Literature';
+--14
+SELECT * FROM year, subject FROM nobel_win WHERE winner = 'Dennis Gabor';
+--15
 
