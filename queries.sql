a7f8e979fb1b4708fb9cd1ebdc6a29bb73cce8d4
NOEL <noel@Mac-mini-de-NOEL.local>
2021-04-04 23:21:57 +0200

    commit_04_04_2021

--- queries.sql
+++ queries.sql
@@ -28,4 +28,61 @@ SELECT * FROM nobel_win WHERE year = 1970 AND category = 'Literature';
 --14
 SELECT * FROM year, subject FROM nobel_win WHERE winner = 'Dennis Gabor';
 --15
-
+SELECT winner 
+FROM nobel_win
+WHERE year>=1950
+AND subject='Physics';
+--16 
+SELECT year, subject, winner, country
+FROM nobel_win
+WHERE subject = 'Chemistry'
+AND year>=1965 AND year<=1975;
+--17
+SELECT * FROM nobel_win
+WHERE year >1972
+AND winner IN ('Menachem Begin',
+                  'Yitzhak Rabin');
+--18
+SELECT * FROM nobel_win
+WHERE winner LIKE 'Louis %';
+--19
+SELECT * FROM nobel_win  WHERE (subject ='Physics' AND year=1970) UNION (SELECT * FROM nobel_win  WHERE (subject ='Economics' AND year=1971));
+--20
+SELECT * FROM nobel_win WHERE year = 1970 AND subject NOT IN ('Physiology', 'Economics');
+--21
+SELECT * FROM nobel_win WHERE (subject ='Physiology' AND year<1971) UNION (SELECT * FROM nobel_win WHERE (subject ='Peace' AND year>=1974));
+--22
+SELECT * FROM nobel_win WHERE winner = 'Johannes Georg Bednorz';
+--23
+SELECT * FROM nobel_win WHERE category NOT LIKE 'P%' ORDER BY year DESC, winner;
+--24
+SELECT *
+FROM nobel_win
+WHERE year=1970 
+ORDER BY
+ CASE
+    WHEN subject IN ('Economics','Chemistry') THEN 1
+    ELSE 0
+ END ASC,
+ subject,
+ winner;
+ --25 
+ SELECT * FROM item_mast WHERE pro_price >= 200 and pro_price <= 600;
+ --26
+ SELECT AVG(pro_price) FROM item_mast WHERE pro_com = 16;
+ --28
+ SELECT pro_name, pro_price WHERE pro_price >= 250 ORDER BY pro_price DESC, pro_name;
+ --29
+ SELECT AVG(pro_price), pro_com
+ FROM item_mast
+ GROUP BY pro_com;
+--30
+SELECT pro_name, pro_price FROM item_mast WHERE pro_price = (SELECT MIN(pro_price) FROM item_mast);
+--31
+SELECT DISTINCT emp_name from emp_details;
+--32
+SELECT * FROM emp_details WHERE emp_name = 'Snares';
+--33
+SELECT * 
+FROM emp_details 
+WHERE emp_dept= 57;
